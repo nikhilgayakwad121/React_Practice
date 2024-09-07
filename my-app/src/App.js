7d700bc02e9996768379a1ca911ba0d1c906912d
Nikhil Gayakwad <nikhilgayakwad121@gmail.com>
2024-09-07 21:21:22 +0530

    Addded react app

---
+++ my-app/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+       <p>
+        we made our first react App
+       </p>
+    </div>
+  );
+}
+
+export default App;
